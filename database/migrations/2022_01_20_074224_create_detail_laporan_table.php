aa81921c98caf46216a6500e53244aa04e1602e7
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-20 14:52:42 +0700

    + migration Detail Laporan

---
+++ database/migrations/2022_01_20_074224_create_detail_laporan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailLaporanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detail_laporan', function (Blueprint $table) {
+            $table->id('id_dtl_laporan');
+            $table->string('transaksi_id');
+            $table->string('paket_laundry_id');
+            $table->integer('jml');
+            $table->integer('harga');
+            $table->integer('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detail_laporan');
+    }
+}
