29872dddad3b5600a7068205f46406f4a44bc35b
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-20 14:51:31 +0700

    + migration Transaksi

---
+++ database/migrations/2022_01_20_073432_create_transaksi_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id('id_transaksi');
+            $table->string('no_transaksi');
+            $table->string('nama_customer');
+            $table->date('tgl_awal');
+            $table->date('tgl_akhir');
+            $table->integer('ongkir');
+            $table->integer('grand_total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
