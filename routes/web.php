18c3e4bc59c354e7167197e399dd86f8fc4275ed
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-21 13:13:29 +0700

    add route [list kategori]

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,5 @@ use Illuminate\Support\Facades\Route;
     //! Paket Laundry 
     Route::get('/paket-laundry', 'PaketLaundryController@index')->name('laundry.index');
     Route::post('/paket-laundry', 'PaketLaundryController@store')->name('laundry.store');
+    Route::get('/paket-laundry/{kategori}', 'PaketLaundryController@listKategori');
 
