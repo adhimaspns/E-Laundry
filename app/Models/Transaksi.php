4429af6e8b5865652f888c30d333559415ec9f61
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-22 11:15:16 +0700

    + protected table, fillable

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaksi extends Model
 {
-    //
+    protected $table        = 'transaksi';
+
+    protected $fillable     = [
+        'no_transaksi',
+        'nama_customer',
+        'tgl_awal',
+        'tgl_akhir',
+        'ongkir',
+        'status',
+        'grand_total'
+    ];
 }
