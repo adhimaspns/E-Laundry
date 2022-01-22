3157ee78bad76a5fe9c2d1a776cc0bea1ce41dd1
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-22 20:59:03 +0700

    + routes [get] transaksi-json

--- routes/web.php
+++ routes/web.php
@@ -29,5 +29,6 @@ use Illuminate\Support\Facades\Route;
     Route::resource('/jenis-laundry', 'JenisLaundryController');
 
     //! Transaksi
-    Route::resource('/transaksi', 'TransaksiController'); 
+    Route::resource('/transaksi', 'TransaksiController');
+    Route::get('transaksi-json', 'TransaksiController@transaksiJson'); 
 
