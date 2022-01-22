7e8235f35027af788f398eae24bbc0df91cbe45d
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-22 22:51:22 +0700

    + fillable [paket]

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -11,6 +11,7 @@ class Transaksi extends Model
     protected $fillable     = [
         'no_transaksi',
         'nama_customer',
+        'paket',
         'tgl_awal',
         'tgl_akhir',
         'ongkir',
