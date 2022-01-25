efcd519da6bf8b71cc29dda96bad8ce8de6bf569
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-25 15:04:15 +0700

    chnge link jquery

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,3 +1,3 @@
 <!-- Bootstrap SCRIPTS -->
-<script src="{{ URL::asset('addon/js/jquery.slim.min.js') }}"></script>
+<script src="{{ URL::asset('addon/datatables/js/jquery-3.5.1.min.js') }}"></script>
 <script src="{{ URL::asset('addon/bootstrap/js/bootstrap.min.js') }}"></script>
