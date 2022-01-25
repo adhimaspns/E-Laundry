24203c2b03fd557a5da1635e60db71ff4e828a3e
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-25 14:58:15 +0700

    + fillable, relasi

--- app/Models/DetailTransaksi.php
+++ app/Models/DetailTransaksi.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetailTransaksi extends Model
 {
-    //
+    protected $table        = 'detail_transaksi';
+
+    protected $fillable     = [
+        'transaksi_id',
+        'jenis_laundry_id',
+        'jml',
+        'harga',
+        'subtotal'
+    ];
+
+    protected $primaryKey = 'id_dtl_transaksi';
+
+    public function jns_lndry()
+    {
+        return $this->belongsTo(JenisLaundry::class, 'jenis_laundry_id', 'id_jns_lndry');
+    }
 }
