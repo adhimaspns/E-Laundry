1aef454dbcc948a2f4354feaf8d4a1a8075b1d7f
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-28 16:02:28 +0700

    chnge link dahsboard

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">E-Laundry</a>
+        <a class="navbar-brand" href="{{ url('/dashboard') }}">E-Laundry</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,7 +8,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item @yield('dashboard')">
-                    <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="{{ url('/dashboard') }}">Dashboard <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item @yield('transaksi')">
                     <a class="nav-link" href="{{ url('transaksi')}}">Transaksi</a>
@@ -26,6 +26,11 @@
                     </div>
                 </li>
             </ul>
+
+            <form action="{{ url('proses-logout') }}" method="POST" class="form-inline my-2 my-lg-0">
+                @csrf
+                <button class="btn btn-sm-block btn-outline-danger my-2 my-sm-0" type="submit">Logout</button>
+            </form>
         </div>
     </div>
 </nav>
