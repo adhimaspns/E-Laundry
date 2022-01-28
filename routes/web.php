02cf8a8ff4b8a494e9a401eb75760345cf999167
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-28 17:44:44 +0700

    add routes AUTH

--- routes/web.php
+++ routes/web.php
@@ -12,36 +12,44 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+    //! AUTH 
+    Route::get('login', 'AuthController@login');
+    Route::post('proses-login', 'AuthController@proses_login');
+    Route::post('proses-logout', 'AuthController@logout');
 
-    Route::get('/', function () {
-        return view('layouts/app');
+    Route::group(['middleware' => ['auth', 'CheckRole:Operator']], function() {
+
+        Route::get('/dashboard', 'DashboardController@index');
+
+        //! Paket Laundry 
+        Route::get('/paket-laundry', 'PaketLaundryController@index')->name('laundry.index');
+        Route::post('/paket-laundry', 'PaketLaundryController@store')->name('laundry.store');
+        Route::get('/paket-laundry/{id}', 'PaketLaundryController@show');
+        Route::get('/paket-laundry/{id}/edit', 'PaketLaundryController@edit');
+        Route::patch('/paket-laundry/{id}', 'PaketLaundryController@update')->name('laundry.update');
+        Route::delete('paket-laundry/{id}', 'PaketLaundryController@destroy')->name('laundry.destroy');
+
+        //! Jenis Laundry
+        Route::resource('/jenis-laundry', 'JenisLaundryController');
+
+        //! Transaksi
+        Route::resource('/transaksi', 'TransaksiController');
+        Route::get('transaksi-json', 'TransaksiController@transaksiJson'); 
+        Route::get('/transaksi/checkout/{no_transaksi}', 'DetailTransaksiController@kasir');
+
+        //! Detail Transaksi
+        Route::resource('detail-transaksi', 'DetailTransaksiController'); 
+        Route::post('checkout', 'DetailTransaksiController@detailTransaksi')->name('checkout'); 
+        Route::get('checkout/{no_transaksi}', 'DetailTransaksiController@checkout');
+        Route::get('checkout/cetak-nota/{no_transaksi}', 'DetailTransaksiController@cetak_nota');
+
+        //! Laporan
+        Route::resource('laporan', 'LaporanController'); 
+        Route::get('laporan-json', 'LaporanController@laporan_json');
+        Route::get('export-excel/{no_transaksi}', 'LaporanController@export_excel');
+        Route::post('export-pdfbydate', 'LaporanController@export_pdf_date');
     });
 
-    //! Paket Laundry 
-    Route::get('/paket-laundry', 'PaketLaundryController@index')->name('laundry.index');
-    Route::post('/paket-laundry', 'PaketLaundryController@store')->name('laundry.store');
-    Route::get('/paket-laundry/{id}', 'PaketLaundryController@show');
-    Route::get('/paket-laundry/{id}/edit', 'PaketLaundryController@edit');
-    Route::patch('/paket-laundry/{id}', 'PaketLaundryController@update')->name('laundry.update');
-    Route::delete('paket-laundry/{id}', 'PaketLaundryController@destroy')->name('laundry.destroy');
+    Auth::routes();
 
-    //! Jenis Laundry
-    Route::resource('/jenis-laundry', 'JenisLaundryController');
-
-    //! Transaksi
-    Route::resource('/transaksi', 'TransaksiController');
-    Route::get('transaksi-json', 'TransaksiController@transaksiJson'); 
-    Route::get('/transaksi/checkout/{no_transaksi}', 'DetailTransaksiController@kasir');
-
-    //! Detail Transaksi
-    Route::resource('detail-transaksi', 'DetailTransaksiController'); 
-    Route::post('checkout', 'DetailTransaksiController@detailTransaksi')->name('checkout'); 
-    Route::get('checkout/{no_transaksi}', 'DetailTransaksiController@checkout');
-    Route::get('checkout/cetak-nota/{no_transaksi}', 'DetailTransaksiController@cetak_nota');
-
-    //! Laporan
-    Route::resource('laporan', 'LaporanController'); 
-    Route::get('laporan-json', 'LaporanController@laporan_json');
-    Route::get('export-excel/{no_transaksi}', 'LaporanController@export_excel');
-    Route::post('export-pdfbydate', 'LaporanController@export_pdf_date');
-
+    Route::get('/home', 'HomeController@index')->name('home');
