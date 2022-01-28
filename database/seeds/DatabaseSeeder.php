5d1c62ddb02bd322db159d121ff5637a54a989b8
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-28 16:56:47 +0700

    add seeding Transaksi, DetailTransaksi

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,9 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
         $this->call(PaketLaundrySeeder::class);
         $this->call(JenisLaundrySeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(TransaksiSeeder::class);
+        $this->call(DetailTransaksiSeeder::class);
     }
 }
