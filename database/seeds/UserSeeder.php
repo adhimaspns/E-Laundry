2b6bf6f9cd833a7b7675abe4ca4f4546ad12cafe
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-28 16:00:26 +0700

    add UserSeeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\User;
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'nama'           => "Operator",
+            'username'       => "opeator",
+            'password'       => bcrypt('operator123'),
+            'level'          => 'Operator',
+            'created_at'     => Carbon::now()
+        ];
+
+        User::insert($user);
+    }
+}
