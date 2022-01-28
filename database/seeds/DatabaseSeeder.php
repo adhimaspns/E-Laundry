4edda420328a398fb77e80f0c22c658440579fa2
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-01-28 16:03:10 +0700

    add seeding UserSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(PaketLaundrySeeder::class);
         $this->call(JenisLaundrySeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
