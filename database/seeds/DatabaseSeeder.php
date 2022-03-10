e9b4ca106630c88e973c2b8749e26486e207b07f
adhimaspns <adhimaspnsmjr1@gmail.com>
2022-03-10 21:15:02 +0700

    seeding Laporan

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(TransaksiSeeder::class);
         $this->call(DetailTransaksiSeeder::class);
+        $this->call(LaporanSeeder::class);
     }
 }
